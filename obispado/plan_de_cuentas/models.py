5fe757d40092402a57cd5197264d81a5a5160afe
lorenafigueredo <lorenafigueredo@b94a5ad2-1791-ed15-fc17-7004231c6ed8>
2010-09-28 23:48:43 +0000

    con admin de django y plan de cuentas incluido en admin

--- obispado/plan_de_cuentas/models.py
+++ obispado/plan_de_cuentas/models.py
@@ -11,18 +11,27 @@ class TipoCuenta(models.Model):
     nombre = models.CharField(max_length=40)
     tipo_de_saldo = models.CharField(max_length=1, choices=TIPO_SALDO_CHOICES)
     # monto = ?
+    def __unicode__(self):
+        return self.nombre
+
 
 class CuentaNivel1(models.Model):
     '''he?'''
     nombre = models.CharField(max_length=40)
     tipo = models.ForeignKey(TipoCuenta)
-    # monto = ?
+    def __unicode__(self):
+        return self.nombre
 
 class CuentaNivel2(models.Model):
     nombre = models.CharField(max_length=40)
     tipo = models.ForeignKey(CuentaNivel1)
+    def __unicode__(self):
+        return self.nombre
 
+	
 class CuentaNivel3(models.Model):
     nombre = models.CharField(max_length=40)
     tipo = models.ForeignKey(CuentaNivel2)
+    def __unicode__(self):
+        return self.nombre
     
