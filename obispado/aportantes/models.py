8eca586e7746a1a675c3c4c82f165b3c4caf56ec
lorenafigueredo <lorenafigueredo@b94a5ad2-1791-ed15-fc17-7004231c6ed8>
2010-09-28 23:14:52 +0000

    Primera subida

---
+++ obispado/aportantes/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Aportante(models.Model):
+	nombre = models.CharField(max_length=40)
+	ruc = models.CharField(max_length=11)
+	#ver si quieren telefono e email
+	
---
+++ obispado/proveedores/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Proveedor(models.Model):
+	nombre = models.CharField(max_length=40)
+	ruc = models.CharField(max_length=11)
+	#ver si quieren telefono e email
+	
