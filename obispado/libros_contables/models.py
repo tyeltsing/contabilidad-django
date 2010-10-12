eb1799faf9771b0c29ddc1e2f2b6d70e6133e27d
sergiogonzalezveron <sergiogonzalezveron@b94a5ad2-1791-ed15-fc17-7004231c6ed8>
2010-10-12 19:07:52 +0000

    Mejorando el modelo de asientos contables

--- obispado/libros_contables/models.py
+++ obispado/libros_contables/models.py
@@ -2,16 +2,26 @@ from django.db import models
 from obispado.plan_de_cuentas.models import CuentaNivel3
 
 class AsientoContable(models.Model):
+    fecha = models.DateField()
     debe = models.ManyToManyField(CuentaNivel3, through='AsientoDebeDetalle', related_name='CuentasDebe')
     haber = models.ManyToManyField(CuentaNivel3, through='AsientoHaberDetalle', related_name='CuentasHaber')
     comentario = models.CharField(max_length=100, null=True)
+
+    def __unicode__(self):
+        return str(self.fecha) + ' - #:' + str(self.id) + ' - ' + self.comentario
 
 class AsientoDebeDetalle(models.Model):
     asiento = models.ForeignKey(AsientoContable)
     cuenta = models.ForeignKey(CuentaNivel3)
     monto = models.FloatField()
 
+    def __unicode__(self):
+        return str(self.asiento) + ' - ' + str(self.id)
+
 class AsientoHaberDetalle(models.Model):
     asiento = models.ForeignKey(AsientoContable)
     cuenta = models.ForeignKey(CuentaNivel3)
     monto = models.FloatField()
+
+    def __unicode__(self):
+        return str(self.asiento) + ' - ' + str(self.id)
