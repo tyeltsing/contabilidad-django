9e83038aa5ecad48ec6036c187f9a20318fa2ddc
lorenafigueredo <lorenafigueredo@b94a5ad2-1791-ed15-fc17-7004231c6ed8>
2010-10-13 12:52:47 +0000

    con template de ingreso y egreso

--- trunk/obispado/ingresos/views.py
+++ trunk/obispado/ingresos/views.py
@@ -4,6 +4,7 @@ from obispado.ingresos.models import *
 from obispado.aportantes.models import *
 from django.http import HttpResponse, HttpResponseRedirect
 from django.db.models import Q, Max, Min
+import datetime, string
 
 def index(request):
     lista_ultimos_ingresos = Venta.objects.all().order_by('-fecha')[:5]
@@ -14,24 +15,44 @@ def carga(request):
         ap = request.GET['ap']
         fe = request.GET['fe']
         ruc = request.GET['ruc']
-        cant = request.GET['cant']
-        des = request.GET['des']
-        pu = request.GET['pu']
-        ex = request.GET['ex']
         if 'tot' in request.GET and request.GET['tot']:
             tot = request.GET['tot']
         else:
             tot = 1000
         #final = ap+fe+ruc+cant+des+pu+ex+tot
+        
+        
         id_aportante = Aportante.objects.filter(nombre=ap)
         valormaximo = Aportante.objects.aggregate(Max('id'))
         valapmax = valormaximo['id__max']
         valapmax = valapmax + 1
         newingreso = Venta(fecha = fe, aportante_id=1)
         newingreso.save()
+        listcant = []
+        listdes = []
+        listpu = []
+        listex = []
+        cont = 0
+        for i in range(1, 11):
+            if 'cant'+str(i) in request.GET and request.GET['cant'+str(i)]:
+                listcant.append(request.GET['cant'+str(i)])
+                cont = cont + 1
+            if 'des'+str(i) in request.GET and request.GET['des'+str(i)]:
+                listdes.append(request.GET['des'+str(i)])
+            if 'pu'+str(i) in request.GET and request.GET['pu'+str(i)]:
+                listpu.append(request.GET['pu'+str(i)])
+            if 'ex'+str(i) in request.GET and request.GET['ex'+str(i)]:
+                listex.append(request.GET['ex'+str(i)])
+            
+        
+        for i in range(0, cont):
+            newventadet = VentaDetalle(venta_id = newingreso.id, cuenta_id = 1, cantidad = listcant[i], exenta = listex[i])
+            newventadet.save()
+        
+        
         #return render_to_response('ingresos/carga_ingreso.html')
-        return HttpResponseRedirect('/carga_ingresos/')
-        #return render_to_response('ingresos/index.html', {'final': final})
+        #return HttpResponseRedirect('/carga_ingresos/')
+        return render_to_response('ingresos/index.html', {'final': cont})
     return render_to_response('ingresos/carga_ingreso.html')
     
 def carga_ingresos(request):
