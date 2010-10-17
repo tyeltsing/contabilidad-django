81056a968ae77d6af7643e41a9ee53b4b981efa0
sergiogonzalezveron <sergiogonzalezveron@b94a5ad2-1791-ed15-fc17-7004231c6ed8>
2010-10-17 17:49:04 +0000

    Mejorando el model MTV. Puse la consulta del balance en models.py

--- obispado/libros_contables/models.py
+++ obispado/libros_contables/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from obispado.plan_de_cuentas.models import CuentaNivel3
+from obispado.plan_de_cuentas.models import *
 
 class AsientoContable(models.Model):
     fecha = models.DateField()
@@ -15,8 +15,8 @@ class AsientoDebeDetalle(models.Model):
     cuenta = models.ForeignKey(CuentaNivel3)
     monto = models.FloatField()
     # UNIQUE TOGHETER, porque no puede repetirse la cuenta en el asiento
-    #class Meta:
-    #    unique_together = (("asiento", "cuenta"),)
+    # class Meta:
+    #     unique_together = (("asiento", "cuenta"),)
 
     def __unicode__(self):
         return str(self.asiento) + ' - ' + str(self.id)
@@ -26,8 +26,64 @@ class AsientoHaberDetalle(models.Model):
     cuenta = models.ForeignKey(CuentaNivel3)
     monto = models.FloatField()
     # UNIQUE TOGHETER, porque no puede repetirse la cuenta en el asiento
-    #class Meta:
-    #    unique_together = (("asiento", "cuenta"),)
+    # class Meta:
+    #     unique_together = (("asiento", "cuenta"),)
 
     def __unicode__(self):
         return str(self.asiento) + ' - ' + str(self.id)
+
+
+# funciones de consulta a la base de datos
+def generar_balance(fecha_desde, fecha_hasta):
+    '''Genera el balance General'''
+    # TODO: Tema de las fechas, como hacer eso?
+    # tal vez se podria pasar el parametro de mes, luego cuando se traen los
+    # saldos de AsientoDebeDetalle y AsientoHaberDetalle filtrar por fecha,
+    # usando filter (date <= fecha_que_me_paso_el_usuario)
+
+    grupos_de_cuentas = TipoCuenta.objects.all()
+    diccionario_balance = {}
+    # Disculpame Kreitmayr por la falta de recursividad en esta parte :)
+    for g in grupos_de_cuentas:
+        diccionario_balance[g] = {'suma': 0}
+        lista_nivel1 = CuentaNivel1.objects.filter(tipo=g)
+        for n1 in lista_nivel1:
+            diccionario_balance[g][n1] = {'suma': 0}
+            lista_nivel2 = CuentaNivel2.objects.filter(tipo=n1)
+            for n2 in lista_nivel2:
+                diccionario_balance[g][n1][n2] = {'suma': 0}
+                lista_nivel3 = CuentaNivel3.objects.filter(tipo=n2)
+                for n3 in lista_nivel3:
+                    diccionario_balance[g][n1][n2][n3] = {'suma': 0}
+
+    # ya tenemos la lista completa de cuentas, ahora debemos calcular el saldo a
+    # la fecha del balance
+    # recorremos nuestro super diccionario de la manera mas ineficaz
+    # TODO: como consultar esto en forma mas eficaz
+    for g in diccionario_balance:
+        if g != 'suma':
+            for n1 in diccionario_balance[g]:
+                if n1 != 'suma':
+                    for n2 in diccionario_balance[g][n1]:
+                        if n2 != 'suma':
+                            for n3 in diccionario_balance[g][n1][n2]:
+                                if n3 != 'suma':
+                                    saldo = 0
+                                    lista_monto_debe_n3 = AsientoDebeDetalle.objects.filter(cuenta=n3)
+                                    suma_monto_debe_n3 = 0
+                                    for i in lista_monto_debe_n3:
+                                        suma_monto_debe_n3 += i.monto
+                                    lista_monto_haber_n3 = AsientoHaberDetalle.objects.filter(cuenta=n3)
+                                    suma_monto_haber_n3 = 0
+                                    for i in lista_monto_haber_n3:
+                                        suma_monto_haber_n3 += i.monto
+                                    saldo = suma_monto_debe_n3 - suma_monto_haber_n3
+                                    if g.tipo_de_saldo == 'h':
+                                        saldo *= -1 # por el tema del saldo, debe y haber, segun Luca Paccioli
+                                    diccionario_balance[g][n1][n2][n3]['suma'] = saldo
+                                    diccionario_balance[g][n1][n2]['suma'] += saldo
+                            diccionario_balance[g][n1]['suma'] += diccionario_balance[g][n1][n2]['suma']
+                    diccionario_balance[g]['suma'] += diccionario_balance[g][n1]['suma']
+    # hasta aqui ya tenemos el balance
+    return diccionario_balance
+
