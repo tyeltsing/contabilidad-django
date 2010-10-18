73908e322fc735c220fb51689bd5b00e6564f672
sergiogonzalezveron <sergiogonzalezveron@b94a5ad2-1791-ed15-fc17-7004231c6ed8>
2010-10-18 00:09:25 +0000

    Mejorando la salida del balance

--- trunk/obispado/balances/views.py
+++ trunk/obispado/balances/views.py
@@ -1,31 +1,58 @@
 from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse
 from obispado.libros_contables.models import *
 from obispado.plan_de_cuentas.models import *
+import csv
 
 def index(request):
     # get_object_or_404(Poll, pk=poll_id)
     return render_to_response('balances/index.html')
 
 def ver_balance(request):
-    # TODO: Tema de las fechas, como hacer eso?
-    # tal vez se podria pasar el parametro de mes, luego cuando se traen los
-    # saldos de AsientoDebeDetalle y AsientoHaberDetalle filtrar por fecha,
-    # usando filter (date <= fecha_que_me_paso_el_usuario)
+    ''' Funcion para mostrar el balance '''
 
-    diccionario_balance = generar_balance(1, 2) # aca le paso dos enteros, pero 
+    balance = generar_balance(1, 2) # aca le paso dos enteros, pero 
                                                 # deberian ser las fechas
     
 
-import csv
-from django.http import HttpResponse
-
 def some_view(request):
+    balance = generar_balance(1, 2) # como se podria llamar mejor a esta funcion? deberia llamarse desde ver_balance, o algo asi
+
     # Create the HttpResponse object with the appropriate CSV header.
     response = HttpResponse(mimetype='text/csv')
     response['Content-Disposition'] = 'attachment; filename=somefilename.csv'
 
     writer = csv.writer(response)
-    writer.writerow(['First row', 'Foo', 'Bar', 'Baz'])
-    writer.writerow(['Second row', 'A', 'B', 'C', '"Testing"', "Here's a quote"])
+    #writer.writerow(['First row', 'Foo', 'Bar', 'Baz'])
+    #writer.writerow(['Second row', 'A', 'B', 'C', '"Testing"', "Here's a quote"])
+    writer.writerow(['Balance']) # titulo
+    writer.writerow(['']) # linea en blanco
+    writer.writerow(['', '', '', '', '', 'Saldos', '', 'Inventario', '', 'Resultado'])
+    writer.writerow(['', 'Concepto', '', '', '', 'Deudor', 'Acreedor', 'Activo', 'Pasivo', 'Debe', 'Haber'])
+    
+    for g in balance:
+        saldo_tipo = g.tipo_de_saldo
+        if saldo_tipo == 'd':
+            writer.writerow(['', str(g.nombre), '', '', '', balance[g]['suma']])
+        else:
+            writer.writerow(['', str(g.nombre), '', '', '', '', balance[g]['suma']])
+        for n1 in balance[g]:
+            if n1 != 'suma':
+                if saldo_tipo == 'd':
+                    writer.writerow(['', '', str(n1.nombre), '', '', str(balance[g][n1]['suma'])])
+                else:
+                    writer.writerow(['', '', str(n1.nombre), '', '', '', str(balance[g][n1]['suma'])])
+                for n2 in balance[g][n1]:
+                    if n2 != 'suma':
+                        if saldo_tipo == 'd':
+                            writer.writerow(['', '', '', str(n2.nombre), '', str(balance[g][n1][n2]['suma'])])
+                        else:
+                            writer.writerow(['', '', '', str(n2.nombre), '', '', str(balance[g][n1][n2]['suma'])])
+                        for n3 in balance[g][n1][n2]:
+                            if n3 != 'suma':
+                                if saldo_tipo == 'd':
+                                    writer.writerow(['', '', '', '', str(n3.nombre), str(balance[g][n1][n2][n3]['suma'])])
+                                else:
+                                    writer.writerow(['', '', '', '', str(n3.nombre), '', str(balance[g][n1][n2][n3]['suma'])])
 
     return response
