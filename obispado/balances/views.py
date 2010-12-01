d62e48db8179b6b0c6eca1e1a65f1c67ae2f1216
lorenafigueredo <lorenafigueredo@b94a5ad2-1791-ed15-fc17-7004231c6ed8>
2010-12-01 05:06:58 +0000

    cambiados datos a int y delimitador del csv

--- obispado/balances/views.py
+++ obispado/balances/views.py
@@ -22,7 +22,7 @@ def some_view(request):
     response = HttpResponse(mimetype='text/csv')
     response['Content-Disposition'] = 'attachment; filename=balance.csv'
 
-    writer = csv.writer(response)
+    writer = csv.writer(response, delimiter=';')
     #writer.writerow(['First row', 'Foo', 'Bar', 'Baz'])
     #writer.writerow(['Second row', 'A', 'B', 'C', '"Testing"', "Here's a quote"])
     writer.writerow(['Balance']) # titulo
@@ -33,26 +33,26 @@ def some_view(request):
     for g in balance:
         saldo_tipo = g.tipo_de_saldo
         if saldo_tipo == 'd':
-            writer.writerow(['', str(g.nombre), '', '', '', balance[g]['suma']])
+            writer.writerow(['', str(g.nombre), '', '', '', int(balance[g]['suma'])])
         else:
-            writer.writerow(['', str(g.nombre), '', '', '', '', balance[g]['suma']])
+            writer.writerow(['', str(g.nombre), '', '', '', '', int(balance[g]['suma'])])
         for n1 in balance[g]:
             if n1 != 'suma':
                 if saldo_tipo == 'd':
-                    writer.writerow(['', '', str(n1.nombre), '', '', str(balance[g][n1]['suma'])])
+                    writer.writerow(['', '', str(n1.nombre), '', '', int(balance[g][n1]['suma'])])
                 else:
-                    writer.writerow(['', '', str(n1.nombre), '', '', '', str(balance[g][n1]['suma'])])
+                    writer.writerow(['', '', str(n1.nombre), '', '', '', int(balance[g][n1]['suma'])])
                 for n2 in balance[g][n1]:
                     if n2 != 'suma':
                         if saldo_tipo == 'd':
-                            writer.writerow(['', '', '', str(n2.nombre), '', str(balance[g][n1][n2]['suma'])])
+                            writer.writerow(['', '', '', str(n2.nombre), '', int(balance[g][n1][n2]['suma'])])
                         else:
-                            writer.writerow(['', '', '', str(n2.nombre), '', '', str(balance[g][n1][n2]['suma'])])
+                            writer.writerow(['', '', '', str(n2.nombre), '', '', int(balance[g][n1][n2]['suma'])])
                         for n3 in balance[g][n1][n2]:
                             if n3 != 'suma':
                                 if saldo_tipo == 'd':
-                                    writer.writerow(['', '', '', '', str(n3.nombre), str(balance[g][n1][n2][n3]['suma'])])
+                                    writer.writerow(['', '', '', '', str(n3.nombre), int(balance[g][n1][n2][n3]['suma'])])
                                 else:
-                                    writer.writerow(['', '', '', '', str(n3.nombre), '', str(balance[g][n1][n2][n3]['suma'])])
+                                    writer.writerow(['', '', '', '', str(n3.nombre), '', int(balance[g][n1][n2][n3]['suma'])])
 
     return response
